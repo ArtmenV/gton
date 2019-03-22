48ba1879af0a88284754ed112d7d183c05ebe9b9
Artmen_V <viliutich.artem@gmail.com>
2019-03-22 23:53:32 +0700

    three commit

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
-const buttonText = 'Click Me!';
-
     return (
-    <div>
-        <label className="label" for="name">
-            Enter name:
-        </label>
-        <input id="name" type="text" />
-        <button style={{ backgroundColor: 'blue', color: 'white' }}>
-            { buttonText }
-        </button>
-     </div>
+        <div className="ui container comments">
+            <CommentDetail 
+                author="Sam"
+                timeAgo="Today at 4:45"
+                content="Nice blog post"
+                avatar="https://fappeningsauce.com/wp-content/uploads/2017/04/MilaKunis_626_1.jpg"
+            />   
+            <CommentDetail 
+                author="Alex"
+                timeAgo="Today at 2:45"
+                content="I like the subject"
+                avatar="https://c.wallhere.com/photos/c4/b2/brunette_brown_eyed_girl_face-1014735.jpg!d"
+            />  
+            <CommentDetail 
+                author="Jane"
+                timeAgo="Today at 3:45"
+                content="I like the writing"
+                avatar="https://avatars.mds.yandex.net/get-zen_doc/1245815/pub_5b010fb31aa80c57154388ac_5b011a891410c3e49332e888/scale_1200"
+            />     
+        </div>
+    
      );
 };
 ReactDOM.render(
     <App />, 
     document.querySelector('#root')
 );
- 
+  
